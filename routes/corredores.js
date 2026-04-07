065257e2a53b7b1259b521805fa5be18855b4814
enzoITs <enzomassaharu@gmail.com>
2026-04-07 08:28:15 -0300

    Terminei as rotas dos corredores

--- routes/corredores.js
+++ routes/corredores.js
@@ -15,14 +15,14 @@ corredoresRoutes.get('/', (req, res) => {
 
 //rota para criar um novo corredor 
 corredoresRoutes.post('/', (req, res) => {
-    const { idcorredores, nome, turma} = req.body;
+    const { nome, turma} = req.body;
 
-    if (!idcorredores || !nome || !turma) {
+    if (!nome || !turma) {
         return res.status(400).json({ error: 'Todos os campos são obrigatórios' });
     }
 
-    const sql = "INSERT INTO corredores (idcorredores, nome, turma) VALUES (?, ?, ?)";
-    db.query(sql, [idcorredores, nome, turma], (err, results) => {
+    const sql = "INSERT INTO corredores (nome, turma) VALUES (?, ?)";
+    db.query(sql, [nome, turma], (err, results) => {
         if (err) {
             console.error('erro ao criar corredor: ', err);
             return res.status(500).json({ error: err.message });
@@ -32,6 +32,42 @@ corredoresRoutes.post('/', (req, res) => {
 });
 
 //rota para atualizar um corredor
+corredoresRoutes.put('/:id', (req, res) => {
+    const { idcorredores, nome, turma } = req.body;
+    const { id } = req.params;
 
+    if (!idcorredores || !nome || !turma) {
+        return res.status(400).json({error: 'Todos os campos são obrigatórios' });
+    }
+
+    const sql = 'UPDATE corredores SET idcorredores = ?, nome = ?, turma = ? WHERE idcorredores = ?'; 
+    db.query(sql, [idcorredores, nome, turma, id], (err, results) => {
+        if (err) {
+            console.error('Erro ao atualizar corredor: ', err);
+            return res.status(500).json({ error: err.message });
+        };
+        if (results.affectedRows === 0) {
+            return res.status(404).json({ error: 'Corredor não encontrado'})
+        }
+        res.json({ menssage: 'Corredor atualizado com sucesso' });
+        })
+})
+
+
+//rota para deletar um corredor
+corredoresRoutes.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    const sql = 'DELETE FROM corredores WHERE idcorredores = ?';
+    db.query(sql, [id], (err, results) => {
+        if (err) {
+            console.error('Erro ao deletar corredor: ', err);
+            return res.status(500).json({ error: err.message });
+        };
+        if (results.affectedRows === 0) {
+            return res.status(404).json({ error: 'Corredor não encontrado'})
+        }
+        res.json({ menssage: 'Corredor deletado com sucesso' });
+        })
+})
 
 module.exports = corredoresRoutes;    
